fdea95240aa7ec346114e463178901b31c6d5ff7
Divyansh Pahariya <divyanshpahariya@gmail.com>
2026-06-03 11:33:17 +0530

    Added navigation and alarm creation screen

--- app/src/main/java/com/divyansh/alarmclockapp/MainActivity.kt
+++ app/src/main/java/com/divyansh/alarmclockapp/MainActivity.kt
@@ -3,15 +3,16 @@ package com.divyansh.alarmclockapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.divyansh.alarmclockapp.ui.theme.AlarmClockAppTheme
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.divyansh.alarmclockapp.ui.theme.AlarmClockAppTheme
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
 
             AlarmClockAppTheme {
